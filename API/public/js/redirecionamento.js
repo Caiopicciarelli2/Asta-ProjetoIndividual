32631859019b3b4dbd966144e5b0d9d5ea416f54
caiopicciarelli2 <caio.psilva@sptech.school>
2026-04-23 11:38:18 -0300

    update configs de redirecionamento

---
+++ API/public/js/redirecionamento.js
@@ -0,0 +1,9 @@
+// funcões de redirecionamento
+
+function redirecionamento_login(){
+    window.location.href = url('login.html');
+}
+
+function redirecionamento_cadastro(){
+    window.location.href = url('cadastro.html');
+}
