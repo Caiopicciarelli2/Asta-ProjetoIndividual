c7001e66ab0dc29fab40a539ca8961d927235305
caiopicciarelli2 <caio.psilva@sptech.school>
2026-04-25 20:25:53 -0300

    Refatorando a home(index.html) inteiro, mudando o design e estrutura da página

---
+++ API/public/js/redirecionamento.js
@@ -0,0 +1,15 @@
+function redirecionamento_login(){
+    window.location.href = "login.html";
+}
+
+function redirecionamento_cadastro(){
+    window.location.href = "cadastro.html";
+}
+
+function redirecionamento_historia(){
+    window.location.href = "simulador.html";
+}
+
+function redirecionamento_quiz(){
+    window.location.href = "quiz.html";
+}
