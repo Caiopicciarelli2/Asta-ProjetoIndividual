55ab022883666159d16ea09d3e650f9a02d7350b
caiopicciarelli2 <caio.psilva@sptech.school>
2026-05-01 19:24:08 -0300

    feature do perfil completa

--- web-data-viz/public/js/index.js
+++ web-data-viz/public/js/index.js
@@ -6,9 +6,20 @@ validarSessao();
 // lógica do perfil
 let container_perfil_user = document.getElementById('perfil-user');
 let icone_perfil = document.getElementById('btn-icone-perfil');
+let btn_fechar_perfil_user = document.getElementById('btn-fechar_perfil');
 
-icone_perfil.addEventListener('click', ()=>{
-    container_perfil_user.style.display = 'flex';
+icone_perfil.addEventListener('click', () => {
+    let display_container = getComputedStyle(container_perfil_user).display;
+
+    if (display_container == 'none') {
+        container_perfil_user.style.display = 'flex';
+    } else {
+        container_perfil_user.style.display = 'none';
+    }
+})
+
+btn_fechar_perfil_user.addEventListener('click', () => {
+    container_perfil_user.style.display = 'none';
 })
 
 
--- web-data-viz/public/js/sessao.js
+++ web-data-viz/public/js/sessao.js
@@ -7,8 +7,12 @@ function validarSessao() {
     // linkando os elementos html da page INDEX
     let ul_navLinks = document.getElementById('nav-links-ul');
     let div_navButtons = document.getElementById('nav-buttons');
+
     let container_sectionLogar = document.getElementById('section-logar');
-    let ul_footerLinks = document.getElementById('footer-links-ul')
+
+    let ul_footerLinks = document.getElementById('footer-links-ul');
+
+    let container_perfilUser = document.getElementById('perfil-user');
 
     // email e nome do BD
     var email = sessionStorage.EMAIL_USUARIO;
@@ -39,7 +43,46 @@ function validarSessao() {
 
         div_navButtons.innerHTML = 
         `
-            <img src="./assets/icones/user-asta.png" style="height: 60px;" title="Clique para ver seu perfil." id="btn-icone-perfil">
+            <img src="./assets/icones/user-asta.png" alt="icone_asta" title="Clique para ver seu perfil." id="btn-icone-perfil">
+        `;
+
+        // comportamento do perfil_user
+
+        container_perfilUser.innerHTML = 
+        `
+            <i class="fa-solid fa-circle-xmark" id="btn-fechar_perfil"></i>
+            <div class="container-perfil-title">
+                <div class="perfil-title-left">
+                    <img src="./assets/icones/user-asta.png" alt="icone_asta" title="Asta">
+                </div>
+                <div class="perfil-title-right">
+                    <h1>${nome}</h1>
+                    <span>Mago do Reino de Clover</span>
+                </div>
+            </div>
+            <div class="divisao-perfil">
+                <span></span>
+                <img src="./assets/icones/logo.png" alt="logo_nav">
+                <span></span>
+
+            </div>
+            <div class="content-perfil">
+                <div class="content-perfil-info">
+                    <i class="fa-solid fa-envelope"></i>
+                    <h6>
+                        Email: 
+                        <span>
+                            ${email}
+                        </span>
+                    </h6>
+                </div>
+                <div class="content-perfil-button">
+                    <button onclick="limparSessao()">
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        Logout
+                    </button>
+                </div>
+            </div>
         `;
 
         // removendo a section de logar
@@ -66,16 +109,15 @@ function validarSessao() {
                 </li>
         `;
 
-
     // se ele não estiver
     } else if (pagina_now == ('/quiz.html' || '/placar.html' || '/status.html' || 'dash.html')) {
-        window.location = "../login.html";
+        window.location = "../index.html";
     }
 }
 
 function limparSessao() {
     sessionStorage.clear();
-    window.location = '../login.html';
+    window.location = '../index.html';
 }
 
 // carregamento (loading)
