b96c8f31837d342ef9e79664d696b6d62f89d251
caiopicciarelli2 <caio.psilva@sptech.school>
2026-05-13 20:49:25 -0300

    upando o front-end do quiz

--- web-data-viz/public/js/quiz.js
+++ web-data-viz/public/js/quiz.js
@@ -1,1 +1,410 @@
-validarSessao();
+// valida a sessão do usuário, ou seja, ve se ele está logado ou não
+
+let validar_sessao_quiz = validarSessao();
+
+console.log(validar_sessao_quiz)
+
+if (validar_sessao_quiz == 'user') {
+
+    // email e nome do BD
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    // linkando os elementos html da page INDEX
+    let ul_navLinks = document.getElementById('nav-links-ul');
+    let div_navButtons = document.getElementById('nav-buttons');
+    let container_perfilUser = document.getElementById('perfil-user');
+
+    // mudando o comportamento da navbar, após o usuário logar ele consegue navegador por todas as páginas
+    ul_navLinks.innerHTML =
+        `
+            <li>
+                <a href="./index.html">Home</a>
+            </li>
+            <li>
+                <a href="./historia.html">História</a>
+            </li>
+            <li>
+                <a class="selected" href="./quiz.html">Quiz</a>
+            </li>
+            <li>
+                <a href="./placar.html">Placar</a>
+            </li>
+            <li>
+                <a href="./status.html">Seu Status</a>
+            </li>
+        `;
+
+    div_navButtons.innerHTML =
+        `
+            <img src="./assets/icones/user-asta.png" alt="icone_asta" title="Clique para ver seu perfil." id="btn-icone-perfil">
+        `;
+
+    // comportamento do perfil_user
+
+    container_perfilUser.innerHTML =
+        `
+            <i class="fa-solid fa-circle-xmark" id="btn-fechar_perfil"></i>
+            <div class="container-perfil-title">
+                <div class="perfil-title-left">
+                    <img src="./assets/icones/user-asta.png" alt="icone_asta" title="Asta">
+                </div>
+                <div class="perfil-title-right">
+                    <h1>${nome}</h1>
+                    <span>Mago do Reino de Clover</span>
+                </div>
+            </div>
+            <div class="divisao-perfil">
+                <span></span>
+                <img src="./assets/icones/logo.png" alt="logo_nav">
+                <span></span>
+
+            </div>
+            <div class="content-perfil">
+                <div class="content-perfil-info">
+                    <i class="fa-solid fa-envelope"></i>
+                    <h6>
+                        Email: 
+                        <span>
+                            ${email}
+                        </span>
+                    </h6>
+                </div>
+                <div class="content-perfil-button">
+                    <button onclick="limparSessao()">
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        Logout
+                    </button>
+                </div>
+            </div>
+        `;
+
+    // lógica do perfil
+    let container_perfil_user = document.getElementById('perfil-user');
+    let icone_perfil = document.getElementById('btn-icone-perfil');
+    let btn_fechar_perfil_user = document.getElementById('btn-fechar_perfil');
+
+    icone_perfil.addEventListener('click', () => {
+        let display_container = getComputedStyle(container_perfil_user).display;
+
+        if (display_container == 'none') {
+            container_perfil_user.style.display = 'flex';
+        } else {
+            container_perfil_user.style.display = 'none';
+        }
+    })
+
+    btn_fechar_perfil_user.addEventListener('click', () => {
+        container_perfil_user.style.display = 'none';
+    })
+
+} else if (validar_sessao_quiz == 'admin') {
+    // email e nome do BD
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    // linkando os elementos html da page INDEX
+    let ul_navLinks = document.getElementById('nav-links-ul');
+    let div_navButtons = document.getElementById('nav-buttons');
+    let container_perfilUser = document.getElementById('perfil-user');
+
+    // mudando o comportamento da navbar, após o usuário logar ele consegue navegador por todas as páginas
+    ul_navLinks.innerHTML =
+        `
+            <li>
+                <a href="./index.html">Home</a>
+            </li>
+            <li>
+                <a href="./historia.html">História</a>
+            </li>
+            <li>
+                <a class="selected" href="./quiz.html">Quiz</a>
+            </li>
+            <li>
+                <a href="./placar.html">Placar</a>
+            </li>
+            <li>
+                <a href="./status.html">Seu Status</a>
+            </li>
+        `;
+
+    div_navButtons.innerHTML =
+        `
+            <img src="./assets/icones/user-asta.png" alt="icone_asta" title="Clique para ver seu perfil." id="btn-icone-perfil">
+            <button class="btn-admin" onclick="redirecionamento_dashboard()">Painel Administrador</button>
+        `;
+
+    // comportamento do perfil_user
+
+    container_perfilUser.innerHTML =
+        `
+            <i class="fa-solid fa-circle-xmark" id="btn-fechar_perfil"></i>
+            <div class="container-perfil-title">
+                <div class="perfil-title-left">
+                    <img src="./assets/icones/user-asta.png" alt="icone_asta" title="Asta">
+                </div>
+                <div class="perfil-title-right">
+                    <h1>${nome}</h1>
+                    <span>Mago do Reino de Clover</span>
+                </div>
+            </div>
+            <div class="divisao-perfil">
+                <span></span>
+                <img src="./assets/icones/logo.png" alt="logo_nav">
+                <span></span>
+
+            </div>
+            <div class="content-perfil">
+                <div class="content-perfil-info">
+                    <i class="fa-solid fa-envelope"></i>
+                    <h6>
+                        Email: 
+                        <span>
+                            ${email}
+                        </span>
+                    </h6>
+                </div>
+                <div class="content-perfil-button">
+                    <button onclick="limparSessao()">
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        Logout
+                    </button>
+                </div>
+            </div>
+        `;
+
+    // lógica do perfil
+    let container_perfil_user = document.getElementById('perfil-user');
+    let icone_perfil = document.getElementById('btn-icone-perfil');
+    let btn_fechar_perfil_user = document.getElementById('btn-fechar_perfil');
+
+    icone_perfil.addEventListener('click', () => {
+        let display_container = getComputedStyle(container_perfil_user).display;
+
+        if (display_container == 'none') {
+            container_perfil_user.style.display = 'flex';
+        } else {
+            container_perfil_user.style.display = 'none';
+        }
+    })
+
+    btn_fechar_perfil_user.addEventListener('click', () => {
+        container_perfil_user.style.display = 'none';
+    })
+}
+
+// quiz
+
+const listaDeQuestoes = [
+
+    {
+        pergunta: "Qual mês tem 30 dias?",
+        alternativaA: "Janeiro",
+        alternativaB: "Dezembro",
+        alternativaC: "Junho",
+        alternativaD: "Agosto",
+        alternativaCorreta: "alternativaC"
+    },
+
+    {
+        pergunta: "Quantas horas tem em um dia?",
+        alternativaA: "30 horas",
+        alternativaB: "38 horas",
+        alternativaC: "48 horas",
+        alternativaD: "24 horas",
+        alternativaCorreta: "alternativaD"
+    },
+
+    {
+        pergunta: "Qual destes números é ímpar?",
+        alternativaA: "Dez",
+        alternativaB: "Doze",
+        alternativaC: "Oito",
+        alternativaD: "Onze",
+        alternativaCorreta: "alternativaD"
+    }
+
+]
+
+// variáveis globais    
+let numeroDaQuestaoAtual = 0
+let pontuacaoFinal = 0
+let tentativaIncorreta = 0
+let certas = 0
+let erradas = 0
+let quantidadeDeQuestoes = listaDeQuestoes.length
+// let isUltima = numeroDaQuestaoAtual == quantidadeDeQuestoes-1 ? true : false
+
+function onloadEsconder() {
+    document.getElementById('pontuacao').style.display = "none"
+    document.getElementById('jogo').style.display = "none"
+}
+
+function iniciarQuiz() {
+    document.getElementById('pontuacao').style.display = "flex"
+    document.getElementById('jogo').style.display = "flex"
+    document.getElementById('btnIniciarQuiz').style.display = "none"
+
+    document.getElementById('qtdQuestoes').innerHTML = quantidadeDeQuestoes
+
+    preencherHTMLcomQuestaoAtual(0)
+
+    btnSubmeter.disabled = false
+    btnProx.disabled = true
+    // btnConcluir.disabled = true
+    btnTentarNovamente.disabled = true
+}
+
+function preencherHTMLcomQuestaoAtual(index) {
+    habilitarAlternativas(true)
+    const questaoAtual = listaDeQuestoes[index]
+    numeroDaQuestaoAtual = index
+    console.log("questaoAtual")
+    console.log(questaoAtual)
+    document.getElementById("spanNumeroDaQuestaoAtual").innerHTML = Number(index) + 1 // ajustando porque o index começa em 0
+    document.getElementById("spanQuestaoExibida").innerHTML = questaoAtual.pergunta;
+    document.getElementById("labelOpcaoUm").innerHTML = questaoAtual.alternativaA;
+    document.getElementById("labelOpcaoDois").innerHTML = questaoAtual.alternativaB;
+    document.getElementById("labelOpcaoTres").innerHTML = questaoAtual.alternativaC;
+    document.getElementById("labelOpcaoQuatro").innerHTML = questaoAtual.alternativaD;
+}
+
+function submeter() {
+    const options = document.getElementsByName("option"); // recupera alternativas no html
+
+    let hasChecked = false
+    for (let i = 0; i < options.length; i++) {
+        if (options[i].checked) {
+            hasChecked = true
+            break
+        }
+    }
+
+    if (!hasChecked) {
+        alert("Não há alternativas escolhidas. Escolha uma opção.")
+    } else {
+        btnSubmeter.disabled = true
+        btnProx.disabled = false
+
+        habilitarAlternativas(false)
+
+        checarResposta()
+    }
+}
+
+function habilitarAlternativas(trueOrFalse) {
+    let opcaoEscolhida = trueOrFalse ? false : true
+
+    primeiraOpcao.disabled = opcaoEscolhida
+    segundaOpcao.disabled = opcaoEscolhida
+    terceiraOpcao.disabled = opcaoEscolhida
+    quartaOpcao.disabled = opcaoEscolhida
+
+}
+
+function avancar() {
+    btnProx.disabled = true
+    btnSubmeter.disabled = false
+
+    desmarcarRadioButtons()
+
+    if (numeroDaQuestaoAtual < quantidadeDeQuestoes - 1) {
+        preencherHTMLcomQuestaoAtual(numeroDaQuestaoAtual)
+    } else if (numeroDaQuestaoAtual == quantidadeDeQuestoes - 1) {
+        alert("Atenção... a próxima é a ultima questão!")
+        preencherHTMLcomQuestaoAtual(numeroDaQuestaoAtual)
+    } else {
+        finalizarJogo()
+    }
+    limparCoresBackgroundOpcoes()
+}
+
+function tentarNovamente() {
+    // atualiza a página
+    window.location.reload()
+}
+
+function checarResposta() {
+    const questaoAtual = listaDeQuestoes[numeroDaQuestaoAtual] // questão atual 
+    const respostaQuestaoAtual = questaoAtual.alternativaCorreta // qual é a resposta correta da questão atual
+
+    const options = document.getElementsByName("option"); // recupera alternativas no html
+
+    let alternativaCorreta = null // variável para armazenar a alternativa correta
+
+    options.forEach((option) => {
+        if (option.value === respostaQuestaoAtual) {
+            console.log("alternativaCorreta está no componente: " + alternativaCorreta)
+            alternativaCorreta = option.labels[0].id
+        }
+    })
+
+    // verifica se resposta assinalada é correta
+    options.forEach((option) => {
+        if (option.checked === true && option.value === respostaQuestaoAtual) {
+            document.getElementById(alternativaCorreta).classList.add("text-success-with-bg")
+            pontuacaoFinal++
+            certas++
+            document.getElementById("spanCertas").innerHTML = certas
+            numeroDaQuestaoAtual++
+        } else if (option.checked && option.value !== respostaQuestaoAtual) {
+            const wrongLabelId = option.labels[0].id
+
+            document.getElementById(wrongLabelId).classList.add("text-danger-with-bg")
+            document.getElementById(alternativaCorreta).classList.add("text-success-with-bg")
+            tentativaIncorreta++
+            erradas++
+            document.getElementById("spanErradas").innerHTML = erradas
+            numeroDaQuestaoAtual++
+        }
+    })
+}
+
+function limparCoresBackgroundOpcoes() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).classList.remove("text-danger-with-bg")
+        document.getElementById(option.labels[0].id).classList.remove("text-success-with-bg")
+    })
+}
+
+function desmarcarRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+function finalizarJogo() {
+    let textoParaMensagemFinal = null
+    let classComCoresParaMensagemFinal = null
+    const porcentagemFinalDeAcertos = pontuacaoFinal / quantidadeDeQuestoes
+
+    if (porcentagemFinalDeAcertos <= 0.3) {
+        textoParaMensagemFinal = "Parece que você não estudou..."
+        classComCoresParaMensagemFinal = "text-danger-with-bg"
+    }
+    else if (porcentagemFinalDeAcertos > 0.3 && porcentagemFinalDeAcertos < 0.9) {
+        textoParaMensagemFinal = "Pode melhorar na próxima, hein!"
+        classComCoresParaMensagemFinal = "text-warning-with-bg"
+    }
+    else if (porcentagemFinalDeAcertos >= 0.9) {
+        textoParaMensagemFinal = "Uau, parabéns!"
+        classComCoresParaMensagemFinal = "text-success-with-bg"
+    }
+
+    textoParaMensagemFinal += "<br> Você acertou " + Math.round((porcentagemFinalDeAcertos) * 100) + "% das questões."
+
+
+    document.getElementById('msgFinal').innerHTML = textoParaMensagemFinal
+    document.getElementById('msgFinal').classList.add(classComCoresParaMensagemFinal)
+    document.getElementById('spanPontuacaoFinal').innerHTML = pontuacaoFinal
+
+    document.getElementById('jogo').classList.add("text-new-gray")
+
+    btnProx.disabled = true
+    btnSubmeter.disabled = true
+    // btnConcluir.disabled = true
+    btnTentarNovamente.disabled = false
+
+}
